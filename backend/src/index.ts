5f6160516cd9bfdd9824c92e7c1f21ac7d381d35
kavinda-100 <kavindarathnayake100@gmail.com>
2025-06-07 20:13:13 +0530

    delete node_modules

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+// home route
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    "message": 'Full MERN Stack - Docker API (TypeScript) is live 🎯'
+  })
+});
+
+// health check route
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({
+    "message": 'Full MERN Stack - Docker API (TypeScript) is healthy 🎯'
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
